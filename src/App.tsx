3988bd2532fde9669ad1009b5710c208b2c3ff1a
HuyPhan1232203 <154007180+HuyPhan1232203@users.noreply.github.com>
2026-02-09 13:39:58 +0700

    Revert "sidebar"
    This reverts commit 38198f39287eb6725987007177af228642fc43a4.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,9 @@
-import { Outlet } from 'react-router-dom'
-import { useStore } from './hooks/use-store'
-import { useSidebar } from './hooks/use-sidebar'
-import { Sidebar } from './components/home-panel/sidebar'
-import { cn } from './lib/utils'
-import Header from './components/home-panel/Header'
+import TiptapEditor from './components/tiptap/TipTapEditor'
 
-export default function App () {
-  const sidebar = useStore(useSidebar, (x) => x)
-
-  if (!sidebar) return null
-  const { getOpenState, settings } = sidebar
-
+const App = () => {
   return (
-    <>
-      <Sidebar />
-      <main
-        className={cn(
-          'min-h-[calc(100vh_-_56px)] bg-zinc-50 dark:bg-zinc-900 transition-[margin-left] ease-in-out duration-300',
-          !settings.disabled && (!getOpenState() ? 'lg:ml-[90px]' : 'lg:ml-72')
-        )}
-      >
-        <Header />
-        <div className='px-4 pb-8 pt-5 '>
-          <Outlet />
-        </div>
-      </main>
-    </>
+    <TiptapEditor />
   )
 }
+
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <App />
 )
